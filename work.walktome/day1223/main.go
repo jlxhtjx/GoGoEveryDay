037f5a7dbd021ff54a84a46d3c340c4c14690b5d
Sunny Tang <jlxhtjx@live.com>
2021-12-23 08:15:23 +0800

    Title: day 1223 start Comments: Go to office now.

---
+++ work.walktome/day1223/main.go
@@ -0,0 +1,9 @@
+package main
+
+/*
+Today is day 2021-12-23, fighting.
+Everything will pass away.
+Sometimes I wonder, why can’t I just chase girls directly?
+Every time I think: why not.
+It's time to study psychology.
+*/
