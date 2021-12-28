0b5be19cdd9243868eb47efe80380fee3e38e2f4
Sunny Tang <jlxhtjx@live.com>
2021-12-28 19:57:02 +0800

    Title: Keep moving. Comments: We need keep moving and will got the happy life.

--- work.walktome/day1226/sunday/opeartors.go
+++ work.walktome/day1226/sunday/opeartors.go
@@ -20,4 +20,44 @@ func main() {
 	a--
 	fmt.Println(a)
 
+	// print bit
+	printBit()
+
+	// print left move and right move.
+	printLeftMove()
+
+	// print value.
+	printVlaue()
+}
+
+/* Need some time to learn about:
+1. Operation system
+2. Computer network
+3. Data structure
+4. Labor Law of the People's Republic of China
+5. SQL
+
+*/
+func printBit() {
+	fmt.Println("----into print bit function.----")
+	a := 1             // 001 in 0101.
+	b := 5             // 101 in 0101.
+	fmt.Println(a & b) // 001 --> 从右往左看，都为1 才是1，so result = 001
+	fmt.Println(a | b) // 101 --> 按位与，有一位为1，那么就是1.
+	fmt.Println(a ^ b) // 100 --> 不一样，就是1，一样就是0.
+
+}
+
+func printLeftMove() {
+	fmt.Println("---left move and right move.----")
+	fmt.Println(1 << 2) // 1 --> 100. result = 4
+	fmt.Println(4 >> 2) //100 --> 1.  result = 1
+
+}
+
+func printVlaue() {
+	fmt.Println("----into print value---")
+	a := 5
+	a += 5
+	fmt.Println(a)
 }
