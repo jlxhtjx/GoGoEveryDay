5bd742c03bc197bf3f9561d69208eed7505ddf77
Sunny Tang <jlxhtjx@live.com>
2021-12-30 00:11:00 +0800

    Title: As I read book tonight Comments:As I read book tonight make me upset and coding make me happy.

--- work.walktome/day1228/t2day/if.go
+++ work.walktome/day1228/t2day/if.go
@@ -12,6 +12,127 @@ func main() {
 	printForWillBreak()
 
 	printSwitch()
+
+	printGotoNormal()
+
+	printGotoSentence()
+
+	printBreakEffect()
+
+	printContinue()
+
+	print9And9()
+}
+
+func print9And9() {
+	fmt.Println("---Print 9 * 9----")
+forLoop1:
+	for i := 1; i <= 9; i++ {
+		for j := 1; j <= 9; j++ {
+			fmt.Printf("%v - %v\t", j, i)
+			if i == j {
+				fmt.Println()
+				continue forLoop1
+			}
+		}
+
+	}
+	fmt.Println("End 9 * 9")
+	// Ok, that's a funny day.
+	// Coding make me happy today.
+
+}
+func printContinue() {
+	fmt.Println("----into continue func.----")
+forloop1:
+	for i := 0; i < 5; i++ {
+		//forloop2
+		for j := 0; j < 5; j++ {
+			if i == 2 && j == 2 {
+				// here is end the for sentence now, and keep going to next for sentence.
+				continue forloop1
+			}
+
+			fmt.Printf("%v - %v\n", i, j)
+		}
+	}
+}
+func printBreakEffect() {
+	fmt.Println("into Break demo.")
+	// This is a code tag? What is code Tag?
+BREAKDEMO1:
+	for i := 0; i < 10; i++ {
+		for j := 0; j < 10; j++ {
+			if j == 2 {
+				break BREAKDEMO1
+			}
+			// What is %v ? no the %d?
+			fmt.Printf("%v - %v\n", i, j)
+			// How about %d?
+			// It seems take the same effect.
+			// fmt.Printf("%d - %d\n", i, j)
+		}
+	}
+	fmt.Println("BREADEMO is end.")
+}
+
+// the way for goto.
+func printGotoSentence() {
+	fmt.Println("into the func: print Goto Sentence.")
+	// and other version can be done by goto sentence.
+	for i := 0; i < 10; i++ {
+		for j := 0; j < 10; j++ {
+			if j == 2 {
+				// set the out flag?
+				// So the goto will run to the tag sentence : breakTag?
+				goto breakTag
+			}
+			fmt.Printf(" %v - %v\n", i, j)
+		}
+	}
+	return
+
+breakTag:
+	fmt.Println("Over the for sentence.")
+	fmt.Println("这是什么操作？我都给看懵了。")
+	goto Hihei
+
+	// I found the goto var must in the one function.
+	// And must take care the order or the goto var.
+	// If you put the Hihei in front of the for sentence, and then it will run forever.
+	// I already try it and stop by myself, So please take care of it.
+Hihei:
+	fmt.Println("最近在读《亲密关系》")
+	/*
+	  亲密关系中的第一章，说到亲密关系是一场修行，每个人都应该珍惜这次修行的机会。
+	  就像每个人都有每个人的使命，东风快递，使命必达。
+	  亲密关系与泛泛之交之间最少存在六个方面的差异：了解，关心，相互依赖性，相互一致性， 信任，以及承诺。
+	  回首望，往往在了解这个过程中就直接 game over了，但是也有人说，如果拿不轨之心去看待朋友，那么就是对爱情和友情的双重污蔑。
+	  想到这一点，在无人的夜晚，又增添了一股孤独感。
+	*/
+	// So goto is very wonderful.
+}
+
+func printGotoNormal() {
+	// here is the normal sentence.
+	var breakFlag bool
+	for i := 0; i < 10; i++ {
+		for j := 0; j < 10; j++ {
+			if j == 2 {
+				// set the out flag.
+				breakFlag = true
+				break
+			}
+			// If here in the sentence has a "\n", and them must using Printf.
+			fmt.Printf(" %v - %v\n", i, j)
+		}
+		// breakFlag must be used.
+		if breakFlag {
+			break
+		}
+
+	}
+
 }
 
 func printSwitch() {
@@ -78,6 +199,24 @@ func printSwitch() {
 		fmt.Println("the switch is bb.")
 	default:
 		fmt.Println("the switch is default.")
+	}
+
+	// switch --> fallthrough 兼容 c 语言。
+	s := "a"
+	switch {
+	case s == "a":
+		fmt.Println("女孩嫁给我")
+		fallthrough //这里就是说  如果满足这个条件了，然后也要执行下一个条件的语句。
+	case s == "b":
+		fmt.Println("买车买房") // 那我当然想要女孩嫁给我咯。 这是 Sunny的私心是也，But the girl says you go away.
+	case s == "c":
+		fmt.Println("女孩不嫁给我")
+		fallthrough
+	case s == "d":
+		fmt.Println("还是单身汉一枚")
+	default:
+		fmt.Println("...")
+
 	}
 
 }
